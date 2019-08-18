ca1d144ed8ba1f2339533deea08e611845c179e5
Liz W <elwainwr@gmail.com>
2019-08-17 22:33:52 -0500

    Add input.html and output_display.html

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,17 @@
+from os import path
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route("/breed_input")
+def fill_in():
+    return render_template("input.html")
+
+@app.route("/breed", methods=["POST", "GET"])
+def breed():
+    if request.method == "POST":
+        result = request.form
+        return render_template("output_display.html", animal=result["Animal"], breed=result["Breed"])
+
+if __name__ == "__main__":
+    app.run(host="10.0.0.178", port=80, debug=True)
